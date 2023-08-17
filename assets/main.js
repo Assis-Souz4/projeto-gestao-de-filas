fc74aabaab4bbb5642df56da616a0b4fb4beba62
Assis Souz4 <assissouza-dev@hotmail.com>
2023-08-17 12:48:03 -0300

    evento form ok

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,35 @@
-const form = document.querySelector('form');
-const resp = document.querySelector('resp');
-const temp = document.querySelector('temp');
+const form = document.querySelector("form");
+const resp = document.querySelector("resp");
+const temp = document.querySelector(".temp");
 const pessoas = [];
+const pessoaPreferencial = 60;
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+const sumirMsg = setTimeout(() => {
+  temp.innerText = "";
+}, 1500);
 
-    const nome = form.inNome.value;
-    const idade = form.inIdade.value;
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    pessoas.push({idade, nome});
+  const nome = form.inNome.value;
+  const idade = form.inIdade.value;
 
-    temp.innerText = `Pessoa cadastrada com sucesso!!`
+  pessoas.push({ nome, idade });
 
-    console.log(nome, idade, pessoas);
+  temp.innerText = `Pessoa cadastrada com sucesso!!`;
+
+  form.reset();
+  form.inNome.focus();
+
+  sumirMsg(temp);
+
+  console.log(nome, idade, pessoas, temp);
+});
+
+form.btnPreferencial.addEventListener("click", () => {
+  if (pessoas.length == 0) {
+    temp.innerText = `Não há pessoas preferenciais!`;
+    sumirMsg();
+    return
+  }
 });
