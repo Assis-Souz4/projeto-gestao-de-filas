210c207c984e88f1b140c4520862991b4ebb46dd
Assis Souz4 <assissouza-dev@hotmail.com>
2023-08-18 00:21:46 -0300

    pendente ajustar bug dos botoes preferencial e comum

--- assets/main.js
+++ assets/main.js
@@ -1,39 +1,77 @@
 const form = document.querySelector("form");
-const resp = document.querySelector("resp");
+const resp = document.querySelector(".resp");
 const temp = document.querySelector(".temp");
-const pessoas = [];
-const pessoasPreferenciais = 60;
+const clientes = [];
 
 function sumirMsg() {
   setTimeout(() => {
     temp.innerText = "";
   }, 1700);
 }
+function arrayVazio() {
+  if (clientes.length == 0) {
+    temp.innerText = "Não existe Clientes prioritários(as)!!";
+    inNome.focus();
+    sumirMsg();
+  }
+}
+
+function prioridade() {
+  arrayVazio();
+  clientes.forEach((cliente) => {
+    // if (cliente.idade < 60) {
+    //   temp.innerText = "Não existe Clientes prioritários(as)!!";
+    //   inNome.focus();
+    //   sumirMsg();
+    // }
+    const preferencial = clientes.filter((cliente) => cliente.idade >= 60, "");
+    resp.innerText = `Cliente(s) com Prioridade para atendimento:\n ${"-".repeat(
+      50
+    )}\n`;
+
+    preferencial.forEach((cliente, i) => {
+      resp.innerText += `${i + 1} - ${cliente.nome}\n`;
+    });
+  });
+}
+function comum() {
+  arrayVazio();
+  clientes.forEach((cliente) => {
+    // if (cliente.idade > 60) {
+    //   temp.innerText = "Não existe Clientes prioritários(as)!!";
+    //   inNome.focus();
+    //   form.reset();
+    //   sumirMsg();
+    // }
+    const comuns = clientes.filter((cliente) => cliente.idade < 60, "");
+    resp.innerText = `Cliente(s) para atendimento:\n ${"-".repeat(50)}\n`;
+
+    comuns.forEach((cliente, i) => {
+      resp.innerText += `${i + 1} - ${cliente.nome}\n`;
+    });
+  });
+}
+function geral() {
+  preferencial();
+}
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const nome = form.inNome.value;
-  const idade = form.inIdade.value;
+  const idade = Number(form.inIdade.value);
 
-  pessoas.push({ nome, idade });
+  clientes.push({ nome, idade });
 
-  temp.innerText = `Pessoa cadastrada com sucesso!!`;
+  temp.innerText = `Clientes cadastrado(a) com sucesso!!`;
 
   form.reset();
   form.inNome.focus();
 
   sumirMsg();
-
-  console.log(nome, idade, pessoas, temp);
+  console.log(clientes);
 });
 
-form.btnPreferencial.addEventListener("click", () => {
+form.btnPreferencial.addEventListener("click", prioridade);
 
-  if (pessoas.idade < 60) {
-    temp.innerText = `Não há pessoas preferenciais!!`;
-    console.log(`Não há pessoas preferenciais!!`);
-    sumirMsg();
-    return;
-  }
-});
+form.btnComum.addEventListener("click", comum);
